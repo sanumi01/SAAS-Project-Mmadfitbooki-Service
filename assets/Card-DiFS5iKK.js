697f3908a08ce91c523a6dfd1c62455c2213f2d4
Kolade Adio <koladeadio@googlemail.com>
2025-11-26 16:55:19 +0000

    Publish site: build 2025-11-26 16:55:18Z

---
+++ assets/Card-DiFS5iKK.js
@@ -0,0 +1,2 @@
+import{j as a}from"./index-B-W2JQhx.js";const o=({children:r,className:s=""})=>a.jsx("div",{className:`card ${s}`,children:r});export{o as C};
+//# sourceMappingURL=Card-DiFS5iKK.js.map
